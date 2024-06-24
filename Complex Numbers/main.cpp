52fc4d1ef4c0cc07a08f1537b9f60c145ef90a8f
Kennia Sanchez <59614236+kennia-sanchez@users.noreply.github.com>
2024-06-24 11:59:26 -0600

    Create main.cpp

---
+++ Complex Numbers/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Complex.h"
+using namespace std;
+
+int main()
+{
+    int iOpcion;
+
+    while(iOpcion != 18)
+    {
+        cout << "Menu" << endl;
+        cout << "1 Input Complex 1" << endl;
+        cout << "2 Input Complex 2" << endl;
+        cout << "3 Print Complex 1" << endl;
+        cout << "4 Print Complex 2" << endl;
+        cout << "5 Add the Complex Numbers" << endl;
+        cout << "6 Substract Complex 1 - Complex 2" << endl;
+        cout << "7 Compare Complex 1 > Complex 2" << endl;
+        cout << "8 Compare Complex 1 < Complex 2" << endl;
+        cout << "9 Compare Complex 1 >= Complex 2" << endl;
+        cout << "10 Compare Complex 1 <= Complex 2" << endl;
+        cout << "11 Compare Complex 1 == Complex 2" << endl;
+        cout << "12 Compare Complex 1 != Complex 2" << endl;
+        cout << "13 Add to Complex 1" << endl;
+        cout << "14 Substract to Complex 2" << endl;
+        cout << "15 Add to Complex 1 the Complex 2" << endl;
+        cout << "16 Substract to Complex 2 the Complex 1" << endl;
+        cout << "17 Finish" << endl;
+        cin >> iOpcion;
+
+        switch (iOpcion)
+        {
+            case 1:
+            
+            break;
+        }
+    }
+} 
